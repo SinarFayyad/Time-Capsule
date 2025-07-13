5edba25dab984830c8dea261d0242ac5b0004832
Sino57 <sinarfayyad5@gmail.com>
2025-07-14 01:26:58 +0300

    search bar component

---
+++ diary/src/Components/Search/search.jsx
@@ -0,0 +1,16 @@
+import styles from './search.module.css'
+import { Search } from 'lucide-react';
+
+const SearchBar = () => {
+
+    return (
+        <div className={styles.search_container}>
+            <div className={styles.search_bar}>
+                <input type="text" placeholder='Search...' className={styles.search}></input>
+                <Search className={styles.search_btn} />
+            </div>
+        </div>
+    );
+}
+
+export default SearchBar
