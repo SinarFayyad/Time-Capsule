ee21d20d50e7426845ca6715a262f0fbb6d0aa86
Sino57 <sinarfayyad5@gmail.com>
2025-07-14 01:27:31 +0300

    message component

---
+++ diary/src/Components/Message/message.jsx
@@ -0,0 +1,17 @@
+import styles from './userMessage.module.css';
+
+const UserMessage = ({ username, message }) => {
+    return (
+        <>
+            <div className={styles.userMessage}>
+                <p className={styles.name}>{username}</p>
+                
+                <br></br>
+                <img src="https://via.placeholder.com/150" alt="img placeholder"></img>
+                <p className={styles.message}>{message}</p>
+            </div>
+        </>
+    );
+}
+
+export default UserMessage
