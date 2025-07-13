76c9f3650ab5e004d64f65e73a75e41577e2a124
Sino57 <sinarfayyad5@gmail.com>
2025-07-14 02:14:15 +0300

    routes

---
+++ diary/src/routes/api.jsx
@@ -0,0 +1,18 @@
+import LandingPage from '../pages/landingPage/landingPage';
+import MainPage from '../pages/mainPage/mainPage'
+import LoginPage from '../pages/loginPage/loginPage'
+import SignUpPage from '../pages/signupPage/signupPage';
+import { Routes, Route } from "react-router-dom";
+
+const MyRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/mainPage" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+    );
+};
+
+export default MyRoutes;
