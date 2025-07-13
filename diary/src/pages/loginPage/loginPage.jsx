ae62a9403528aa7af2e91843039197c315ac87e3
Sino57 <sinarfayyad5@gmail.com>
2025-07-14 02:13:41 +0300

    login page

---
+++ diary/src/pages/loginPage/loginPage.jsx
@@ -0,0 +1,40 @@
+import styles from './loginPage.module.css'
+import { useNavigate } from 'react-router-dom'
+import { Mail, LockKeyhole } from 'lucide-react'
+
+const LoginPage = () => {
+    const navigate = useNavigate();
+    return (
+
+        <div className={styles.login_container}>
+            <div className={styles.content}>
+                <div>
+                    <h1 className={styles.title}>Login</h1>
+                    <div className={styles.labels}>
+                        <Mail />
+                        <label>Email</label>
+                    </div>
+                    <input type='text' placeholder='e.g. JoeDoe@gmail.com'></input>
+
+                    <div className={styles.labels}>
+                        <LockKeyhole />
+                        <label>Password</label>
+                    </div>
+                    <input type='password' placeholder='********'></input>
+
+                    <button onClick={() => { navigate("/mainPage") }} className={styles.login_btn}>Login</button>
+                </div>
+            </div>
+            <div className={styles.secondSection}>
+                <div className={styles.circle}>
+                    <a onClick={() => { navigate("/signUp") }}><h1 className={styles.title}>Sign up</h1></a>
+                    <p className={styles.sentence}>You don't have an account?</p>
+                </div>
+            </div>
+
+        </div>
+
+    );
+}
+
+export default LoginPage
