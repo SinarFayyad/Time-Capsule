89cfaf9b54f5f91b73b69aff4b02191e6d28398a
Sino57 <sinarfayyad5@gmail.com>
2025-07-14 23:51:57 +0300

    add message

--- diary/src/pages/addMessage/addMessage.jsx
+++ diary/src/pages/addMessage/addMessage.jsx
@@ -1,16 +1,31 @@
 import styles from './addMessage.module.css'
 import Navbar from '../../Components/Navbar/navbar';
-import {Smile} from 'lucide-react'
- 
+import { Smile, Meh, Frown ,Paperclip} from 'lucide-react'
+
 const AddMessage = () => {
     return (
         <div className={styles.container}>
             <Navbar />
             <div className={styles.message}>
-                <p>Choose your mood</p>
-                
+                <div className={styles.header}>
+                    <p>Choose your mood</p>
+                    <div className={styles.mood}>
+                        <Smile />
+                        <Meh />
+                        <Frown />
+                    </div>
+                </div>
+
+                <div className={styles.divider}></div>
+
+                <div className={styles.content}>
+                    <textarea className={styles.textarea} placeholder="Type your message here..."></textarea>
+                    <Paperclip className={styles.paperclip}/>
+                </div>
             </div>
 
         </div>
     );
 }
+
+export default AddMessage
