43549b338118a2048b1c4c0629f7801511350718
Sino57 <sinarfayyad5@gmail.com>
2025-07-14 11:54:37 +0300

    sign up page

--- diary/src/pages/signupPage/signupPage.jsx
+++ diary/src/pages/signupPage/signupPage.jsx
@@ -1,22 +1,29 @@
 import styles from './signupPage.module.css'
 import { useNavigate } from 'react-router-dom'
-import { Mail, LockKeyhole } from 'lucide-react'
+import { UserRound, Mail, LockKeyhole } from 'lucide-react'
 
 const SignUpPage = () => {
     const navigate = useNavigate();
     return (
 
         <div className={styles.container}>
-            <div className={styles.secondSection}>
-                <div className={styles.circle}>
-                    <a onClick={() => { navigate("/login") }}><h1 className={styles.title}>Login</h1></a>
-                    <p className={styles.sentence}>You don't have an account?</p>
+            <a onClick={() => { navigate("/login") }}>
+                <div className={styles.secondSection}>
+                    <div className={styles.circle}>
+                        <h1 className={styles.title}>Login</h1>
+                        <p className={styles.sentence}>You don't have an account?</p>
+                    </div>
                 </div>
-            </div>
+            </a>
 
             <div className={styles.content}>
                 <div>
                     <h1 className={styles.title}>Sign Up</h1>
+                    <div className={styles.labels}>
+                        <UserRound />
+                        <label>Username</label>
+                    </div>
+                    <input type='text' placeholder='Joe Doe'></input>
                     <div className={styles.labels}>
                         <Mail />
                         <label>Email</label>
@@ -32,7 +39,7 @@ const SignUpPage = () => {
                     <button onClick={() => { navigate("/mainPage") }} className={styles.btn}>Sign Up</button>
                 </div>
             </div>
-            
+
 
         </div>
 
