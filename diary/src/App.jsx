c8a5cd971c7e55070a95f20850bc3c8e991a98d3
Sino57 <sinarfayyad5@gmail.com>
2025-07-15 01:03:31 +0300

    changes

--- diary/src/App.jsx
+++ diary/src/App.jsx
@@ -1,13 +1,6 @@
 import MyRoutes from './routes/api'
-<<<<<<< Updated upstream
-import './App.css'
-=======
-<<<<<<< Updated upstream
-=======
 import './App.css'
 import './styles/colors.css'
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
 
 const App = () => {
