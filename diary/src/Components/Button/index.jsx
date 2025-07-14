4000fffea63ea174c62e193bcf7ed25f76802ca3
Sino57 <sinarfayyad5@gmail.com>
2025-07-15 01:00:06 +0300

    changes

--- diary/src/Components/Button/index.jsx
+++ diary/src/Components/Button/index.jsx
@@ -1,1 +1,12 @@
 import styles from './style.module.css'
+import { useNavigate } from 'react-router-dom';
+
+const Button = ({path ='/', title }) => {
+
+    const navigate = useNavigate();
+    return (
+        <button onClick={() => { navigate({path}) }} className={styles.btn}>{title}</button>
+    );
+}
+
+export default Button
