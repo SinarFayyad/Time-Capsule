f70e02c5e867d131edea1fef59a49a9da5d8a68b
Sino57 <sinarfayyad5@gmail.com>
2025-07-15 00:00:23 +0300

    edit navbar

--- diary/src/Components/Navbar/navbar.jsx
+++ diary/src/Components/Navbar/navbar.jsx
@@ -1,9 +1,10 @@
 import styles from './navbar.module.css';
-import logo from '../../assets/diary_logo_transparent.png';
-import { useLocation } from "react-router-dom";
+import logo from '../../assets/Time_Capsule_logo.png';
+import { useNavigate , useLocation } from "react-router-dom";
 
 const Navbar = () => {
 
+    const navigate= useNavigate();
     const location = useLocation();
 
     return (
@@ -12,12 +13,12 @@ const Navbar = () => {
                 <img src={logo} alt="logo" className={styles.logo} />
             </div>
             <div className={styles.title}>
-                <h1>Dear Diary...</h1>
+                <h1>Dear Future Me...</h1>
             </div>
             {location.pathname !== '/' &&
                 <div className={styles.links}>
-                    <h3>Add message</h3>
-                    <h3>My messages</h3>
+                    <a onClick = {() => {navigate('/addMessage')}}><h3>Add message</h3></a>
+                    <a onClick = {() => {navigate('/yourMessages')}}><h3>My messages</h3></a>
                 </div>}
         </div>
     );
