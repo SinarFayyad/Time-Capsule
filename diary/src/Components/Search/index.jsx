2c682ed404f46dcd77e7129c85e440099dd307cc
Sino57 <sinarfayyad5@gmail.com>
2025-07-16 01:54:28 +0300

    changes

--- diary/src/Components/Search/index.jsx
+++ diary/src/Components/Search/index.jsx
@@ -6,7 +6,13 @@ const SearchBar = () => {
     return (
         <div className={styles.search_container}>
             <div className={styles.search_bar}>
-                <Input name="search" type="text" hint='Search...' className={`${styles.search}`}/>
+                <Input name="search" 
+                       type="text" 
+                       hint='Search...' 
+                       className={`${styles.search}` }
+                       required="required" 
+                       onChangeListener={console.log('hi')}/> 
+                       {/*fetch info based on mood/country */}
                 <Search className={styles.search_btn} />
             </div>
         </div>
