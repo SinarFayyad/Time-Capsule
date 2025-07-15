77edea91bde487addff701234f0c94aa1e66c521
Sino57 <sinarfayyad5@gmail.com>
2025-07-16 00:05:02 +0300

    component update

--- diary/src/Components/UserMessage/index.jsx
+++ diary/src/Components/UserMessage/index.jsx
@@ -1,12 +1,12 @@
 import styles from './style.module.css';
 
 
-const UserMessage = ({ username, message }) => {
+const UserMessage = ({ content }) => {
     return (
         <>
             <div className={`${styles.userMessage} border`}>
                 <img src="https://via.placeholder.com/150" alt="img placeholder"></img>
-                <p className={styles.message}>{message}</p>
+                <p className={styles.message}>{content}</p>
             </div>
         </>
     );
