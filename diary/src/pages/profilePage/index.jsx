6fd12671eaccd923a428f2e3e66008fbb2baefd4
Sino57 <sinarfayyad5@gmail.com>
2025-07-22 02:01:01 +0300

    add

---
+++ diary/src/pages/profilePage/index.jsx
@@ -0,0 +1,13 @@
+import styles from './style.module.css'
+import Navbar from '../../Components/Navbar/index';
+
+const AddMessage = () => {
+    return (
+        <div className={styles.container}>
+            <Navbar />
+            
+        </div>
+    );
+}
+
+export default AddMessage
