6707a070392c7628bb4f0ad7fe0d7247d6fbd560
Sino57 <sinarfayyad5@gmail.com>
2025-07-22 15:42:43 +0300

    edit messages

--- diary/src/pages/Messages/index.jsx
+++ diary/src/pages/Messages/index.jsx
@@ -3,13 +3,18 @@ import React, { useEffect, useState } from "react";
 import Message from "../../Components/Message/index";
 import styles from './style.module.css'
 
-const Messages = () => {
+const Messages = (content) => {
   const [Messages, setMessages] = useState([]);
 
   const loadMessages = async () => {
-    const res = await axios.get("http://localhost:8000/api/messages/4");
+    let res;
+    if (content == "myMessages") {
+      res = await axios.get("http://localhost:8000/api/messages/2");
+    } else if (content== "allMessages"){
+      res = await axios.get("http://localhost:8000/api/messages")
+    } 
     let message = setMessages(res.data.payload);
-  };
+  }
 
   useEffect(() => {
     loadMessages();
@@ -17,10 +22,10 @@ const Messages = () => {
 
   return (
     <div className={styles.container}>
-        {Messages.map((message) => (
-          <Message title={message.title} message={message.message} media={message.media} mood={message.mood} />
-        ))}
-    </div> 
+      {Messages.map((message) => (
+        <Message title={message.title} message={message.message} media={message.media} mood={message.mood} />
+      ))}
+    </div>
   );
 };
 
