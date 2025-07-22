e01d0297cb2daab9d945113ef9a084d4c1ba16b1
Sino57 <sinarfayyad5@gmail.com>
2025-07-22 14:54:42 +0300

    edits

--- diary/src/Components/Button/index.jsx
+++ diary/src/Components/Button/index.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const Button = ({ title , className, onClickListener}) => {
 
     return (
-        <button className={` ${className}`} onClick={onClickListener} >{title}</button>
+        <button className={`${className}`} onClick={onClickListener} >{title}</button>
     );
 }
 
