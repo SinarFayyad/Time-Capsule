457aadb407f304d1ef66d81c00b0df2ece6d0d15
Sino57 <sinarfayyad5@gmail.com>
2025-07-22 21:17:54 +0300

    edit msg

--- diary/src/pages/Messages/index.jsx
+++ diary/src/pages/Messages/index.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Message from "../../Components/Message/index";
+import Button from "../../Components/Button/index";
+import { useNavigate } from "react-router-dom";
 import styles from './style.module.css'
 
-const Messages = ({content}) => {
+const Messages = ({ content }) => {
   const [Messages, setMessages] = useState([]);
+  const navigate = useNavigate();
 
   const loadMessages = async () => {
     if (content == "myMessages") {
@@ -12,10 +15,10 @@ const Messages = ({content}) => {
       const id = localStorage.getItem("id");
       const res = await axios.get(`http://localhost:8000/api/messages/${id}`);
       let message = setMessages(res.data.payload);
-    } else if (content== "allMessages"){
+    } else if (content == "allMessages") {
       const res = await axios.get("http://localhost:8000/api/messages")
       let message = setMessages(res.data.payload);
-    } 
+    }
   }
 
   useEffect(() => {
@@ -24,9 +27,19 @@ const Messages = ({content}) => {
 
   return (
     <div className={styles.container}>
-      {Messages.map((message) => (
-        <Message title={message.title} message={message.message} media={message.media} mood={message.mood} />
-      ))}
+      {Messages.length === 0 ? (
+        <div className={styles.emptyStateContainer}>
+          <Button title="Create your first message" onClickListener={() => {navigate("/addMessage")}} className={`main-color text-color`}/>
+        </div>
+      ) : (
+        Messages.map((message) => (
+          <Message key={message.id}
+            title={message.title}
+            message={message.message}
+            media={message.media}
+            mood={message.mood} />
+        ))
+      )}
     </div>
   );
 };
